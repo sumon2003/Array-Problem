814271ea0a3f29c3cca09fc7bb0cfaf60d81d71b
sumon2003 <121485281+sumon2003@users.noreply.github.com>
2023-11-18 22:52:05 +0600

    Update Array Reverse.c

--- Array Reverse.c
+++ Array Reverse.c
@@ -1,3 +1,14 @@
+/*
+Write a program in C to store elements in an array and print them.
+Test Data :
+Input 10 elements in the array :
+element - 0 : 1
+element - 1 : 1
+element - 2 : 2
+.......
+Expected Output :
+Elements in array are: 1 1 2 3 4 5 6 7 8 9
+*/
 #include<stdio.h>
 //#include<string.h>
 //#include<ctype.h>
