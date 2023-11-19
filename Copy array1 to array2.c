d8c87b1a260ac6a0c04a17a57f320d7283707a39
sumon2003 <121485281+sumon2003@users.noreply.github.com>
2023-11-19 09:51:32 +0600

    Update Copy array1 to array2

--- Copy array1 to array2.c
+++ Copy array1 to array2.c
@@ -1,3 +1,17 @@
+/*
+Write a program in C to copy the elements of one array into another array.
+Test Data :
+Input the number of elements to be stored in the array :3
+Input 3 elements in the array :
+element - 0 : 15
+element - 1 : 10
+element - 2 : 12
+Expected Output :
+The elements stored in the first array are :
+15 10 12
+The elements copied into the second array are :
+15 10 12
+*/
 #include<stdio.h>
 //#include<string.h>
 //#include<ctype.h>
