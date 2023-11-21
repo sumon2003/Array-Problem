617006667ac4792c16017d502872c3dcee5e102f
Md Sumon Hossain Khan <121485281+sumon2003@users.noreply.github.com>
2023-11-21 07:38:02 +0600

    Count duplicate elements in an array

---
+++ Array Duplicate Number.c
@@ -0,0 +1,36 @@
+/*
+Write a program in C to count the total number of duplicate elements in an array.
+Test Data :
+Input the number of elements to be stored in the array :3
+Input 3 elements in the array :
+element - 0 : 5
+element - 1 : 1
+element - 2 : 1
+Expected Output :
+Total number of duplicate elements found in the array is : 1
+*/
+#include<stdio.h>
+int main(){
+    int n;
+    printf("Input the number of elements to be stored in the array : ");
+    scanf("%d",&n);
+    printf("Input %d elements in the array :\n",n);
+    int arr[n];
+    for(int i=0; i<n; i++){
+        printf("element - %d : ",i);
+        scanf("%d",&arr[i]);
+    }
+    int count_duplicate = 0;
+    for(int i=0; i<n; i++){
+        for(int j=i+1; j<n; j++){
+            if(arr[i]==arr[j]){
+               count_duplicate++;
+               break;
+            }
+        }
+    }
+    printf("Total number of duplicate elements found in the array is : %d\n",count_duplicate);
+
+    
+  return 0;
+}
