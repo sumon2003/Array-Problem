ba29dd2c101909dcd7833742439ae5606b9e8dbb
Md Sumon Hossain Khan <121485281+sumon2003@users.noreply.github.com>
2023-12-24 20:40:18 +0600

    2D Array Declare and User Input

---
+++ 2D Array User Input.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int arr[3][3];
+    printf("Enter the value of array:\n");
+    for(int i=0; i<3; i++){
+        for(int j=0; j<3; j++){
+            scanf("%d",&arr[i][j]);
+        }
+    }
+
+//2D array user input
+return 0;
+}
