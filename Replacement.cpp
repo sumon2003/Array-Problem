1923d2560d5bf87e8a5b7007a4a8e0720a08de9a
Md Sumon Hossain Khan <121485281+sumon2003@users.noreply.github.com>
2024-01-07 23:31:08 +0600

    Replacement

---
+++ Replacement.cpp
@@ -0,0 +1,30 @@
+/*//  بِسْمِ اللهِ الرَّحْمٰنِ الرَّحِيْمِ  //*/
+/*//   Md Sumon Hossain Khan   //*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    int n;
+    cin >> n;
+
+    int arr[n];
+    for(int i=0; i<n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    for(int i=0; i<n; i++)
+    {
+        if(arr[i] < 0) cout << "2 ";
+        else if(arr[i] > 0) cout << "1 ";
+        else cout << "0 ";
+    }
+
+
+
+
+
+    return 0;
+}
