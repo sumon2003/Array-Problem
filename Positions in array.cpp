ec1ad3cc3c531433d1eb48ea94680f2b5b12ea31
Md Sumon Hossain Khan <121485281+sumon2003@users.noreply.github.com>
2024-01-07 23:31:35 +0600

    Position in an Array

---
+++ Positions in array.cpp
@@ -0,0 +1,26 @@
+/*//  بِسْمِ اللهِ الرَّحْمٰنِ الرَّحِيْمِ  //*/
+/*//   Md Sumon Hossain Khan   //*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    int n;
+    cin >> n;
+
+    int arr[n];
+    for(int i=0; i<n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    for(int i=0; i<n; i++)
+    {
+        if(arr[i] > 10) continue;
+        cout << "A[" << i << "] = " << arr[i] << endl;
+    }
+
+
+    return 0;
+}
