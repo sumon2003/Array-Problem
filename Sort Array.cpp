8173e483576061f1a6980b7e7e1d774c36ca0f68
Md Sumon Hossain Khan <121485281+sumon2003@users.noreply.github.com>
2024-01-08 22:06:49 +0600

    Sort Array

---
+++ Sort Array.cpp
@@ -0,0 +1,32 @@
+/*//  بِسْمِ اللهِ الرَّحْمٰنِ الرَّحِيْمِ  //*/
+/*//   Md Sumon Hossain Khan   //*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    int n;
+    cin >> n;
+
+    //int arr[n];
+    vector <int> arr(n);
+    for(int i=0; i<n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    sort(arr.begin(), arr.end());
+
+    for(int i=0; i<n; i++)
+    {
+        cout << arr[i] << " " ;
+    }
+    
+
+
+
+
+
+    return 0;
+}
